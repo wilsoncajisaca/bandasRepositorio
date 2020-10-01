39d21bcf457d65620f142b8451b9431198651f54
Wilson <wilsoncajisaca@gmail.com>
2020-10-01 07:41:32 -0500

    subiendo primera secion del curso

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+//app de express
+const app = express();
+
+//NodeServer
+const server = require('http').createServer(app);
+module.exports.io = require('socket.io')(server);
+require('./sockets/socket');
+
+//path publico
+const publicPath = path.resolve( __dirname, 'public');
+
+app.use(express.static(publicPath));
+
+server.listen(process.env.PORT,(error)=>{
+
+    if(error)throw new Error(error);
+
+    console.log('Servidor corriendo',process.env.PORT);
+
+});
