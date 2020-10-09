7580d5b413fda9ba983fd15a544d5ee0949d8575
Wilson <wilsoncajisaca@gmail.com>
2020-10-09 01:12:57 -0500

    ffff

--- src/controllers/project_controller.js
+++ src/controllers/project_controller.js
@@ -1,26 +1,19 @@
-const Project = require('../models/project');
 const { response } = require('express');
+const { Project } = require('../models/project');
 
-const getProjects = async(req, res = response) => {
+const createProjects = (req, res = response) => {
     //console.log(req.body);
     const { name, priority, remark, deliverydate } = req.body;
     try {
 
-        let newProject = await Project.create({
+        let newProject = Project.create({
             name,
             priority,
             remark,
             deliverydate
+        }, {
+            fields: ['name', 'priority', 'remark', 'deliverydate']
         });
-
-        // let newProject = await Project.create({
-        //     name,
-        //     priority,
-        //     remark,
-        //     deliverydate
-        // }, {
-        //     fields: ['name', 'priority', 'remark', 'deliverydate']
-        // });
 
         if (newProject) {
             return res.json({
@@ -28,11 +21,6 @@ const getProjects = async(req, res = response) => {
                 data: newProject
             });
         }
-        // res.json({
-        //     message: 'Hoooola',
-        //     data: {}
-
-        // });
     } catch (err) {
         console.log(err);
         res.status(500).json({
@@ -43,5 +31,5 @@ const getProjects = async(req, res = response) => {
 }
 
 module.exports = {
-    getProjects
+    createProjects
 }
--- src/routes/project.js
+++ src/routes/project.js
@@ -1,8 +1,7 @@
 const { Router } = require('express');
-const { getProjects } = require('../controllers/project_controller');
+const { createProjects } = require('../controllers/project_controller');
 const router = Router();
 
-
-router.post('/', getProjects);
+router.post('/add', createProjects);
 
 module.exports = router;
