f585bd328434bfcf63dbaf77fb04596f75a35033
Wilson <wilsoncajisaca@gmail.com>
2020-10-09 00:33:14 -0500

    fff

--- src/controllers/project_controller.js
+++ src/controllers/project_controller.js
@@ -5,6 +5,14 @@ const getProjects = async(req, res = response) => {
     //console.log(req.body);
     const { name, priority, remark, deliverydate } = req.body;
     try {
+
+        let newProject = await Project.create({
+            name,
+            priority,
+            remark,
+            deliverydate
+        });
+
         // let newProject = await Project.create({
         //     name,
         //     priority,
@@ -14,17 +22,17 @@ const getProjects = async(req, res = response) => {
         //     fields: ['name', 'priority', 'remark', 'deliverydate']
         // });
 
-        // if (newProject) {
-        //     return res.json({
-        //         message: 'Projecto creado',
-        //         data: newProject
-        //     });
-        // }
-        res.json({
-            message: 'Hoooola',
-            data: {}
+        if (newProject) {
+            return res.json({
+                message: 'Projecto creado',
+                data: newProject
+            });
+        }
+        // res.json({
+        //     message: 'Hoooola',
+        //     data: {}
 
-        });
+        // });
     } catch (err) {
         console.log(err);
         res.status(500).json({
